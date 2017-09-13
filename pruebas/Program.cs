7f86320994407c7c3236cc7268d5a91898eeeeef
Erik Rodriguez <erik.rodrigu3z@gmail.com>
2017-09-12 23:50:42 -0600

    redondar
    round C#

---
+++ pruebas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double redondeo = 123.123456789012345;
+            Console.WriteLine("Redondeo: " + redondeo);            
+
+            for (int i = 1; i <= 15; i++)
+            {
+                Console.WriteLine("Redondeo: #"+ i +" = " + Math.Round(redondeo, i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
